8640376d3f2853c9e85d7a797176679bc85f50a0
Martin Neundorfer <m.neundorfer@labor.digital>
2020-03-25 21:41:33 +0100

    feat(StandaloneBetterQuery): add findRelated() method to find related records for a query + make sure the translation and versioning is applied to query results

--- Classes/Domain/BetterQuery/StandaloneBetterQuery.php
+++ Classes/Domain/BetterQuery/StandaloneBetterQuery.php
@@ -21,14 +21,33 @@ declare(strict_types=1);
 
 namespace LaborDigital\Typo3BetterApi\Domain\BetterQuery;
 
+use LaborDigital\Typo3BetterApi\Container\TypoContainer;
 use LaborDigital\Typo3BetterApi\Domain\BetterQuery\Adapter\DoctrineQueryAdapter;
+use LaborDigital\Typo3BetterApi\Domain\DbService\DbService;
+use LaborDigital\Typo3BetterApi\Page\PageService;
 use LaborDigital\Typo3BetterApi\TypoContext\TypoContext;
+use Neunerlei\Arrays\Arrays;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\Database\RelationHandler;
 use TYPO3\CMS\Extbase\Persistence\Generic\QuerySettingsInterface;
 use TYPO3\CMS\Extbase\Persistence\Generic\Session;
 
 class StandaloneBetterQuery extends AbstractBetterQuery {
+	
 	/**
+	 * The instance of the page repository after it was requested
+	 * @var \TYPO3\CMS\Frontend\Page\PageRepository
+	 */
+	protected $pageRepository;
+	
+	/**
+	 * True if the version overlay should be applied for the query result
+	 * @var bool
+	 */
+	protected $versionOverlay = TRUE;
+	
+	/**
+	 * Creates a new query object
 	 *
 	 * @param string                                                        $tableName
 	 * @param \TYPO3\CMS\Core\Database\Query\QueryBuilder                   $queryBuilder
@@ -39,6 +58,26 @@ class StandaloneBetterQuery extends AbstractBetterQuery {
 	public function __construct(string $tableName, QueryBuilder $queryBuilder, QuerySettingsInterface $settings,
 								TypoContext $typoContext, Session $session) {
 		parent::__construct(new DoctrineQueryAdapter($tableName, $queryBuilder, $settings), $typoContext, $session);
+	}
+	
+	/**
+	 * Sets the flag that determines if the version/workspace overlay should be applied or not; TRUE by default
+	 *
+	 * @param bool $state
+	 *
+	 * @return $this
+	 */
+	public function withVersionOverlay(bool $state) {
+		$this->versionOverlay = $state;
+		return $this;
+	}
+	
+	/**
+	 * Returns true if the version/workspace overlay is used, false if not
+	 * @return bool
+	 */
+	public function useVersionOverlay(): bool {
+		return $this->versionOverlay;
 	}
 	
 	/**
@@ -59,7 +98,10 @@ class StandaloneBetterQuery extends AbstractBetterQuery {
 	 * @return array
 	 */
 	public function getAll() {
-		return $this->getQueryBuilder()->execute()->fetchAll();
+		return array_map(function (array $row) {
+			$tableName = $this->adapter->getTableName();
+			return $this->handleTranslationAndVersionOverlay($tableName, $row);
+		}, $this->getQueryBuilder()->execute()->fetchAll());
 	}
 	
 	/**
@@ -73,12 +115,12 @@ class StandaloneBetterQuery extends AbstractBetterQuery {
 	/**
 	 * Returns the first element from the queries result set that matches your criteria
 	 *
-	 * @param bool
-	 *
 	 * @return mixed
 	 */
 	public function getFirst() {
-		return $this->getQueryBuilder()->execute()->fetch();
+		$result = $this->getQueryBuilder()->execute()->fetch();
+		if (is_array($result)) $result = $this->handleTranslationAndVersionOverlay($this->adapter->getTableName(), $result);
+		return $result;
 	}
 	
 	/**
@@ -109,6 +151,136 @@ class StandaloneBetterQuery extends AbstractBetterQuery {
 	 */
 	public function update(array $values) {
 		return $this->getQueryBuilder()->update($this->adapter->getTableName())->values($values, TRUE)->execute();
+	}
+	
+	/**
+	 * Finds related records for a field in the queried table.
+	 * The result is an array for all relations of the given field for every result of the query.
+	 *
+	 * Translation overlays will be automatically applied.
+	 *
+	 * @param array|string $field                 Either a single field you want to query the relations for or a list
+	 *                                            of fields as an array.
+	 * @param bool         $includeHiddenChildren Set this to true if you want to include hidden children into your
+	 *                                            result
+	 *
+	 * @return array Returns either a list of entries per field name or a list of entries when only a single field is
+	 *               given. The list of entries is ordered by the name of the foreign table.
+	 * @throws \LaborDigital\Typo3BetterApi\Domain\BetterQuery\BetterQueryException
+	 * @see \LaborDigital\Typo3BetterApi\Domain\BetterQuery\RelatedRecordRow
+	 */
+	public function findRelated($field, bool $includeHiddenChildren = FALSE): array {
+		if ($isSingleField = is_string($field)) $field = [$field];
+		if (!is_array($field))
+			throw new BetterQueryException("Only strings and arrays are allowed as \$field!");
+		
+		// Prepare the configuration
+		$qb = $this->getQueryBuilder();
+		$table = $this->adapter->getTableName();
+		$tcaConfig = Arrays::getPath($GLOBALS, ["TCA", $table, "columns", $field, "config"]);
+		if (!is_array($tcaConfig)) throw new BetterQueryException(
+			"The requested field: \"$field\" was not found in the TCA of table: \"$table\"!");
+		
+		// Query the results from the database
+		$records = (clone $qb)->select("uid", ...$field)->execute()->fetchAll();
+		if (empty($records)) return [];
+		
+		// Lazy load additional dependencies
+		$container = TypoContainer::getInstance();
+		$dbService = $container->get(DbService::class);
+		
+		// Iterate the configuration for the fields
+		$resultsByField = [];
+		$additionalWhereCache = [];
+		foreach ($tcaConfig as $currentField => $config) {
+			
+			// Get the table definition for the tca type
+			$mmTable = isset($config["MM"]) ? $config["MM"] : "";
+			$tableList = "";
+			if (isset($config["type"]) && $config["type"] === "group")
+				$tableList = isset($config["allowed"]) ? $config["allowed"] : "";
+			else if (isset($config["foreign_table"]))
+				$tableList = $config["foreign_table"];
+			if (empty($tableList))
+				throw new BetterQueryException("Could not retrieve the foreign tables from the TCA!");
+			
+			// Resolve the relations for every element
+			foreach ($records as $result) {
+				// Create the relation handler
+				$relationHandler = $container->get(RelationHandler::class);
+				$relationHandler->setFetchAllFields(TRUE);
+				$relationHandler->start(empty($mmTable) ? $result[$currentField] : "",
+					$tableList, $mmTable, $result["uid"], $this->adapter->getTableName(), $config);
+				
+				// Generate additional constraints for every table
+				// This is done so we can apply the frontend constraints to the backend utility we use
+				foreach ($relationHandler->tableArray as $localTable => $items) {
+					$additionalWhere = isset($additionalWhereCache[$localTable]) ?
+						$additionalWhereCache[$localTable] :
+						$additionalWhereCache[$localTable] =
+							$dbService->getQuery($localTable)
+								->withLanguage(FALSE)
+								->withIncludeHidden($includeHiddenChildren)
+								->getQueryBuilder()->getSQL();
+					$additionalWhere = " AND " . end(explode("WHERE", $additionalWhere));
+					$relationHandler->additionalWhere[$localTable] = $additionalWhere;
+				}
+				
+				// Request the database using the backend relation handler
+				$relations = $relationHandler->getFromDB();
+				
+				// Handle Overlays
+				foreach ($relations as $localTable => $rows)
+					foreach ($rows as $k => $row)
+						$relations[$localTable][$k] = $this->handleTranslationAndVersionOverlay($localTable, $row);
+				
+				// Generate objects that are in order by their sorting
+				$relationList = [];
+				foreach ($relationHandler->itemArray as $item) {
+					if (!isset($relations[$item["table"]]) || !isset($relations[$item["table"]][$item["id"]])) continue;
+					$relationList[] = new RelatedRecordRow(
+						(int)$item["id"], $item["table"], $relations[$item["table"]][$item["id"]]);
+				}
+				
+				// Store the relations
+				$resultsByField[$currentField][$result["uid"]] = $relationList;
+			}
+		}
+		
+		// Check if we got a single field request
+		if ($isSingleField) return isset($resultsByField[reset($field)]) ? $resultsByField[reset($field)] : [];
+		
+		// Done
+		return $resultsByField;
+	}
+	
+	/**
+	 * Internal helper to handle translation and version overlays of a single row of a given database table
+	 *
+	 * @param string $tableName
+	 * @param array  $row
+	 *
+	 * @return array
+	 */
+	protected function handleTranslationAndVersionOverlay(string $tableName, array $row): array {
+		// Create page repository if required
+		if (empty($this->pageRepository))
+			$this->pageRepository = TypoContainer::getInstance()->get(PageService::class)->getPageRepository();
+		
+		// Apply the version overlay
+		if ($this->versionOverlay)
+			$this->pageRepository->versionOL($tableName, $row, TRUE);
+		
+		// Apply the translation overlay only if required
+		if (!$this->adapter->getSettings()->getRespectSysLanguage()) return $row;
+		$languageUid = $this->adapter->getSettings()->getLanguageUid();
+		if ($languageUid < 0) return $row;
+		
+		// This is basically a copy of the logic in PageRepository->getLanguageOverlay()
+		if (!Arrays::hasPath($GLOBALS, ["TCA", $tableName, "ctrl", "languageField"])) return $row;
+		if ($tableName === "pages") return $this->pageRepository->getPageOverlay($row, $languageUid);
+		return $this->pageRepository->getRecordOverlay($tableName, $row, $languageUid,
+			is_string($this->adapter->getSettings()->getLanguageOverlayMode()) ? "hideNonTranslated" : "1");
 	}
 }
 
