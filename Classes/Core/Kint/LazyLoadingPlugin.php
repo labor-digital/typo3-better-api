d8bfdf836b82bb3a9d357aacb77ee04c12f0fa9c
Martin Neundorfer <m.neundorfer@labor.digital>
2020-08-27 22:05:43 +0200

    feat: migrate Kint debugger TYPO3 bridge

--- Classes/Core/Kint/LazyLoadingPlugin.php
+++ Classes/Core/Kint/LazyLoadingPlugin.php
@@ -1,5 +1,6 @@
 <?php
-/**
+declare(strict_types=1);
+/*
  * Copyright 2020 LABOR.digital
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,18 +15,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Last modified: 2020.03.19 at 01:46
+ * Last modified: 2020.08.23 at 23:23
  */
 
-namespace LaborDigital\Typo3BetterApi\Kint;
+
+namespace LaborDigital\T3BA\Core\Kint;
 
 use Kint\Object\BasicObject;
 use Kint\Object\InstanceObject;
 use Kint\Parser\IteratorPlugin;
 use Kint\Parser\Parser;
 use Kint\Parser\Plugin;
-use LaborDigital\Typo3BetterApi\Container\TypoContainer;
-use LaborDigital\Typo3BetterApi\LazyLoading\LazyLoading;
+use LaborDigital\T3BA\Core\Util\LazyLoadingUtil;
 use Neunerlei\Arrays\Arrays;
 use TYPO3\CMS\Extbase\Persistence\Generic\LazyLoadingProxy;
 use TYPO3\CMS\Extbase\Persistence\Generic\LazyObjectStorage;
@@ -36,48 +37,44 @@ class LazyLoadingPlugin extends Plugin
      * @var \Kint\Parser\Parser
      */
     protected $parser;
-    
+
     public function getTypes()
     {
         return ['object'];
     }
-    
+
     public function getTriggers()
     {
         return Parser::TRIGGER_BEGIN;
     }
-    
+
     public function parse(&$variable, BasicObject &$o, $trigger)
     {
         if ($variable instanceof LazyObjectStorage) {
             /** @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage $realVar */
-            $realVar = $this->getLazyLoading()->getRealValue($variable);
+            $realVar      = LazyLoadingUtil::getRealValue($variable);
+            $realVarArray = Arrays::makeFromObject($realVar);
+
             /** @var InstanceObject $object */
             $object = InstanceObject::blank($o->name);
             $object->transplant($o);
             $object->classname = get_class($realVar);
             $object->depth     = $o->depth + 1;
-            
-            $object2 = InstanceObject::blank($o->name);
-            $object2 = $this->parser->parse(Arrays::makeFromObject($realVar), $object2);
-            $object->addRepresentation(reset($object2->getRepresentations()));
+
+            $object2         = InstanceObject::blank($o->name);
+            $object2         = $this->parser->parse($realVarArray, $object2);
+            $representations = $object2->getRepresentations();
+            $object->addRepresentation(reset($representations));
             $o                           = $object;
             $o->type                     = 'object';
             $o->size                     = $realVar->count();
             IteratorPlugin::$blacklist[] = $object->classname;
         } elseif ($variable instanceof LazyLoadingProxy) {
-            $realVar = $this->getLazyLoading()->getRealValue($variable);
+            $realVar = LazyLoadingUtil::getRealValue($variable);
             $object  = BasicObject::blank($o->name);
             $object->transplant($o);
             $object->depth = $o->depth;
             $o             = $this->parser->parse($realVar, $object);
         }
-        
-        return;
-    }
-    
-    protected function getLazyLoading(): LazyLoading
-    {
-        return TypoContainer::getInstance()->get(LazyLoading::class);
     }
 }
--- Classes/Core/Kint/TypoInstanceTypePlugin.php
+++ Classes/Core/Kint/TypoInstanceTypePlugin.php
@@ -1,5 +1,6 @@
 <?php
-/**
+declare(strict_types=1);
+/*
  * Copyright 2020 LABOR.digital
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,17 +15,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Last modified: 2020.03.16 at 18:42
+ * Last modified: 2020.08.23 at 23:23
  */
 
-namespace LaborDigital\Typo3BetterApi\Kint;
+namespace LaborDigital\T3BA\Core\Kint;
 
 use Kint\Object\BasicObject;
 use Kint\Object\InstanceObject;
 use Kint\Parser\Parser;
 use Kint\Parser\Plugin;
-use LaborDigital\Typo3BetterApi\Container\TypoContainer;
-use LaborDigital\Typo3BetterApi\LazyLoading\LazyLoading;
+use LaborDigital\T3BA\Core\Util\LazyLoadingUtil;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 use TYPO3\CMS\Extbase\Persistence\Generic\LazyLoadingProxy;
 use TYPO3\CMS\Extbase\Persistence\Generic\LazyObjectStorage;
@@ -33,7 +33,7 @@ use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 
 class TypoInstanceTypePlugin extends Plugin
 {
-    
+
     /**
      * @inheritDoc
      */
@@ -41,12 +41,13 @@ class TypoInstanceTypePlugin extends Plugin
     {
         return ['object'];
     }
-    
+
     public function getTriggers()
     {
         return Parser::TRIGGER_COMPLETE;
     }
-    
+
+    /** @noinspection ReferencingObjectsInspection */
     public function parse(&$variable, BasicObject &$o, $trigger)
     {
         // Show the iterator first
@@ -55,34 +56,29 @@ class TypoInstanceTypePlugin extends Plugin
             $o->removeRepresentation('iterator');
             $o->addRepresentation($r, 0);
         }
-        
+
         // Add the uid of entities to the output
         if ($variable instanceof AbstractEntity && $o instanceof InstanceObject) {
             $o->classname = $o->classname .= ' - UID: ' . $variable->getUid();
         }
-        
+
         // Remove the iterator representation from lazy objects
         if ($variable instanceof LazyObjectStorage || $variable instanceof LazyLoadingProxy) {
             $o->removeRepresentation('iterator');
         }
-        
+
         // Add the real class name to lazy loading proxies
         if ($variable instanceof LazyLoadingProxy) {
-            $realVal = $this->getLazyLoading()->getRealValue($variable);
-            $o->classname = $o->classname .= ' - ' . get_class($realVal) .
-                                             ($realVal instanceof AbstractEntity ? ' - UID: ' . $realVal->getUid()
-                                                 : '');
+            $realVal = LazyLoadingUtil::getRealValue($variable);
+            if ($realVal !== null) {
+                $o->classname .= ' - ' . get_class($realVal) .
+                                 ($realVal instanceof AbstractEntity ? ' - UID: ' . $realVal->getUid() : '');
+            }
         }
-        
+
         // Update size for countable objects
-        if ($variable instanceof LazyObjectStorage || $variable instanceof ObjectStorage
-            || $variable instanceof QueryResultInterface) {
+        if ($variable instanceof ObjectStorage || $variable instanceof QueryResultInterface) {
             $o->size = $variable->count();
         }
-    }
-    
-    protected function getLazyLoading(): LazyLoading
-    {
-        return TypoContainer::getInstance()->get(LazyLoading::class);
     }
 }
