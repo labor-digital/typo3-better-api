5b9b2c9f97d07329119f31a68ad93367ffccdb91
Martin Neundorfer <m.neundorfer@labor.digital>
2020-11-01 22:24:25 +0100

    feat(NamingUtil): implement v9 adjustments

--- Classes/Tool/OddsAndEnds/NamingUtil.php
+++ Classes/Tool/OddsAndEnds/NamingUtil.php
@@ -21,7 +21,13 @@ declare(strict_types=1);
 namespace LaborDigital\T3BA\Tool\OddsAndEnds;
 
 use InvalidArgumentException;
+use LaborDigital\T3BA\Tool\TypoContext\TypoContext;
 use Neunerlei\PathUtil\Path;
+use ReflectionMethod;
+use TYPO3\CMS\Core\Utility\ClassNamingUtility;
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+use TYPO3\CMS\Extbase\Persistence\Generic\Mapper\DataMapper;
+use TYPO3\CMS\Extbase\Persistence\Repository;
 
 class NamingUtil
 {
@@ -97,46 +103,133 @@ class NamingUtil
      * Receives the ext key, which may include a vendor like "vendor.my_extension". If it contains a vendor, "vendor"
      * will be returned. If an extKey like "my_extension" is passed, an empty string is returned instead.
      *
-     * @param   string  $extkey
+     * @param   string  $extKey
      *
      * @return string
      */
-    public static function vendorFromExtKey(string $extkey): string
+    public static function vendorFromExtKey(string $extKey): string
     {
-        if (strpos($extkey, '.') === false) {
+        if (strpos($extKey, '.') === false) {
             return '';
         }
 
-        return substr($extkey, 0, strpos($extkey, '.'));
+        return substr($extKey, 0, strpos($extKey, '.'));
+    }
+
+
+    /**
+     * Tries to find the correct sql database table for the given selector.
+     *
+     * IMPORTANT: This method requires a TypoScript as well as the TCA to be set up!
+     *
+     * @param   string|AbstractEntity|Repository  $selector  The selector to find the database table for:
+     *                                                       The valid options are:
+     *                                                       - a string already containing a table name -> will be
+     *                                                       kept as is
+     *                                                       - the class|instance of a domain model
+     *                                                       - the class|instance of a domain repository
+     *
+     * @return string
+     */
+    public static function resolveTableName($selector): string
+    {
+        if (is_object($selector)) {
+            if ($selector instanceof BetterRepository) {
+                return $selector->getTableName();
+            }
+            if ($selector instanceof Repository || $selector instanceof AbstractEntity) {
+                $selector = get_class($selector);
+            }
+        }
+
+        if (is_string($selector)) {
+            if (class_exists($selector)) {
+                // Resolve repository class
+                if (in_array(Repository::class, class_parents($selector), true)) {
+                    $selector = ClassNamingUtility::translateRepositoryNameToModelName($selector);
+                }
+
+                // Resolve entity class
+                if (in_array(AbstractEntity::class, class_parents($selector), true)) {
+                    return TypoContext::getInstance()->Di()->getInstanceOf(DataMapper::class)
+                                      ->getDataMap($selector)->getTableName();
+                }
+            }
+
+            return $selector;
+        }
+
+        throw new InvalidArgumentException('Could not convert the given selector: ' . $selector
+                                           . ' into a database table name!');
     }
 
     /**
-     * Receives a typo callback like namespace\\class->method
-     * and converts it into an array of ["class"=>"namespace\\class", "method" => "method"]
+     * Resolves the given callable into an actual callable.
      *
-     * @param   string  $callback  The callback to be parsed
+     * @param   string|array|callable  $callable               Allowed values are:
+     *                                                         - callable types -> will be passed through
+     *                                                         - TYPO3 callables like namespace\\class->method
+     *                                                         - instantiatable callables [Class::class, 'method']
+     *                                                         Class will be instantiated using the container if
+     *                                                         "method" is not declared "static".
+     * @param   bool                   $instantiateIfRequired  Set this to false if you don't want to instantiate the
+     *                                                         class of instantiable callbacks. Mostly for legacy
+     *                                                         support.
      *
-     * @return array
+     * @return callable
      */
-    public static function typoCallbackToArray(string $callback): array
+    public static function resolveCallable($callable, bool $instantiateIfRequired = true): callable
     {
-        // Concatinated, multi callbacks -> flexform
-        if (strpos($callback, ';') !== false) {
-            return array_map(
-                [static::class, 'typoCallbackToArray'],
-                array_filter(array_map('trim', explode(';', $callback)))
-            );
-        }
-        $callbackParts = explode('->', $callback);
-        if (count($callbackParts) !== 2) {
-            throw new InvalidArgumentException(
-                'Invalid TypoCallback given: "' . $callback . '". It has to be something like: namespace\\class->method'
-            );
+        if (is_string($callable)) {
+            // Return something that is already callable
+            if (is_callable($callable)) {
+                return $callable;
+            }
+
+            // Resolve a list of multiple callables -> FlexForms
+            if (strpos($callable, ';') !== false) {
+                return array_map(
+                    [static::class, __FUNCTION__],
+                    array_filter(array_map('trim', explode(';', $callable)))
+                );
+            }
+
+            // Resolve typo callable
+            if (strpos($callable, '->') !== false) {
+                $parts = explode('->', $callable);
+                if (count($parts) !== 2) {
+                    throw new InvalidArgumentException(
+                        'Invalid callback given: "' . $callable
+                        . '". It has to be something like: namespace\\class->method'
+                    );
+                }
+
+                $callable = [
+                    trim(str_replace('/', '\\', $parts[0]), '\\ '),
+                    trim($parts[1], ' \\/()'),
+                ];
+            }
         }
 
-        return [
-            'class'  => trim(str_replace('/', '\\', $callbackParts[0]), '\\ '),
-            'method' => trim($callbackParts[1], ' \\/()'),
-        ];
+        if (is_array($callable) && count($callable) === 2) {
+            // Already a callable using an instance, or instantiation disabled
+            if (is_callable($callable) && (is_object($callable[0]) || ! $instantiateIfRequired)) {
+                return $callable;
+            }
+
+            // Check if we have to instantiate the class first
+            if (! (new ReflectionMethod($callable[0], $callable[1]))->isStatic()) {
+                return [
+                    TypoContext::getInstance()->Di()->getInstanceOf($callable[0]),
+                    $callable[1],
+                ];
+            }
+        }
+
+        if (! is_callable($callable)) {
+            throw new InvalidArgumentException('Could not resolve the given callable into an actual callable!');
+        }
+
+        return $callable;
     }
 }
