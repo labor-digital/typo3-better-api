cbf93b742282d9d8b19fd4cbdc753bdc1f009b72
Martin Neundorfer <m.neundorfer@labor.digital>
2021-06-01 11:45:21 +0200

    feat(RequestCacheKeyGenerator): add $trackedHeaders and $excludedQueryParams options

--- Classes/Tool/Cache/KeyGenerator/RequestCacheKeyGenerator.php
+++ Classes/Tool/Cache/KeyGenerator/RequestCacheKeyGenerator.php
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Last modified: 2021.04.29 at 22:17
+ * Last modified: 2021.06.01 at 11:36
  */
 
 declare(strict_types=1);
@@ -23,11 +23,12 @@ declare(strict_types=1);
 namespace LaborDigital\T3ba\Tool\Cache\KeyGenerator;
 
 
+use LaborDigital\T3ba\Core\Di\NoDiInterface;
 use LaborDigital\T3ba\Tool\TypoContext\TypoContext;
 use Neunerlei\Arrays\Arrays;
 use Psr\Http\Message\ServerRequestInterface;
 
-class RequestCacheKeyGenerator implements CacheKeyGeneratorInterface
+class RequestCacheKeyGenerator implements CacheKeyGeneratorInterface, NoDiInterface
 {
     /**
      * The list of request headers that should be taken into account when the cache key is generated
@@ -42,13 +43,29 @@ class RequestCacheKeyGenerator implements CacheKeyGeneratorInterface
     protected $request;
     
     /**
+     * The list of request headers that should be taken into account (this combines the ones under static::$trackedHeaders, and the given $tracked headers in the constructor)
+     *
+     * @var array
+     */
+    protected $_trackedHeaders;
+    
+    /**
+     * The list of query parameters that should be excluded when the cache key is generated
+     *
+     * @var array
+     */
+    protected $_excludedQueryParams;
+    
+    /**
      * RequestCacheKeyGenerator constructor.
      *
      * @param   \Psr\Http\Message\ServerRequestInterface  $request
      */
-    public function __construct(ServerRequestInterface $request)
+    public function __construct(ServerRequestInterface $request, ?array $trackedHeaders = null, ?array $excludedQueryParams = null)
     {
         $this->request = $request;
+        $this->_trackedHeaders = array_merge(static::$trackedHeaders, $trackedHeaders ?? []);
+        $this->_excludedQueryParams = $excludedQueryParams ?? [];
     }
     
     /**
@@ -56,24 +73,29 @@ class RequestCacheKeyGenerator implements CacheKeyGeneratorInterface
      */
     public function makeCacheKey(): string
     {
-        $params = Arrays::flatten($this->request->getQueryParams());
+        $typoContext = TypoContext::getInstance();
+        $request = $this->request;
+        
+        $params = Arrays::flatten($request->getQueryParams());
         ksort($params);
         
-        $headers = [];
-        foreach (static::$trackedHeaders as $header) {
-            $headers[$header] = $this->request->getHeaderLine($header);
+        foreach ($this->_excludedQueryParams as $excludedQueryParam) {
+            unset($params[$excludedQueryParam]);
         }
         
-        $typoContext = TypoContext::getInstance();
+        $headers = [];
+        foreach ($this->_trackedHeaders as $trackedHeader) {
+            $headers[$trackedHeader] = $request->getHeaderLine($trackedHeader);
+        }
         
         return md5(implode('-', [
             \GuzzleHttp\json_encode($params),
             \GuzzleHttp\json_encode($headers),
-            $this->request->getMethod(),
-            $this->request->getUri()->getPath(),
-            $typoContext->Language()->getCurrentFrontendLanguage()->getTwoLetterIsoCode(),
-            $typoContext->Pid()->getCurrent(),
-            $typoContext->Site()->getCurrent()->getIdentifier(),
+            $request->getMethod(),
+            $request->getUri()->getPath(),
+            $typoContext->language()->getCurrentFrontendLanguage()->getTwoLetterIsoCode(),
+            $typoContext->pid()->getCurrent(),
+            $typoContext->site()->getCurrent()->getIdentifier(),
         ]));
     }
     
