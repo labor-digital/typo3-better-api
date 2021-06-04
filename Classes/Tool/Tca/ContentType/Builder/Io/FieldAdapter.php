d8da9f9a30cc7f6157cc856f927bf8e4c5bb0c38
Martin Neundorfer <m.neundorfer@labor.digital>
2021-06-04 21:13:59 +0200

    fix(ContentType\FieldAdapter): remove unnecessary NoDiInterface

--- Classes/Tool/Tca/ContentType/Builder/Io/FieldAdapter.php
+++ Classes/Tool/Tca/ContentType/Builder/Io/FieldAdapter.php
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Last modified: 2021.05.10 at 19:00
+ * Last modified: 2021.06.04 at 21:13
  */
 
 declare(strict_types=1);
@@ -23,11 +23,10 @@ declare(strict_types=1);
 namespace LaborDigital\T3ba\Tool\Tca\ContentType\Builder\Io;
 
 
-use LaborDigital\T3ba\Core\Di\NoDiInterface;
 use LaborDigital\T3ba\Tool\Tca\Builder\Logic\AbstractField;
 use LaborDigital\T3ba\Tool\Tca\Builder\Tree\Node;
 
-class FieldAdapter extends AbstractField implements NoDiInterface
+class FieldAdapter extends AbstractField
 {
     /**
      * Returns the logic node of a given field instance
