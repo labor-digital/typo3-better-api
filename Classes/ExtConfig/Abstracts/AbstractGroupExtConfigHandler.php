0fe386d79b833363c2e962293f08cca812fb9e47
Martin Neundorfer <m.neundorfer@labor.digital>
2021-06-12 17:23:34 +0200

    feat(AbstractGroupExtConfigHandler): make getElementKeyForClass() $postProcessor more reliable

--- Classes/ExtConfig/Abstracts/AbstractGroupExtConfigHandler.php
+++ Classes/ExtConfig/Abstracts/AbstractGroupExtConfigHandler.php
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Last modified: 2021.04.29 at 22:17
+ * Last modified: 2021.06.11 at 17:18
  */
 
 declare(strict_types=1);
@@ -38,8 +38,13 @@ abstract class AbstractGroupExtConfigHandler extends AbstractGroupConfigHandler 
      * for classes implementing the ElementKeyProviderInterface
      *
      * @param   string         $class            The class to generate the element key for
-     * @param   callable|null  $postProcessor    An optional post processor which will be executed if the class
-     *                                           does not implement the ElementKeyProviderInterface
+     * @param   callable|null  $postProcessor    An optional post processor which can alter the processing.
+     *                                           It will receive the following arguments:
+     *                                           string $elementClass - The original class name
+     *                                           string $elementKey - The generated element key
+     *                                           bool $hasElementKeyProvider - True if the class provided the element key itself.
+     *                                           This means you should be able to safely ignore the key.
+     *                                           The callback MUST return the $elementKey, even if it did not modify it.
      * @param   bool           $useOnlyBaseName  By default only the class basename is used for key generation.
      *                                           If you set this to false, the whole class including the namespace will
      *                                           be used
@@ -53,18 +58,18 @@ abstract class AbstractGroupExtConfigHandler extends AbstractGroupConfigHandler 
         bool $useOnlyBaseName = true
     ): string
     {
+        $elementKey = $class;
         if (in_array(ElementKeyProviderInterface::class, class_implements($class), true)) {
-            return call_user_func([$class, 'getElementKey']);
-        }
-        
-        if ($useOnlyBaseName) {
-            $class = Path::classBasename($class);
+            $elementKey = call_user_func([$class, 'getElementKey']);
+            $hasElementKeyProvider = true;
+        } elseif ($useOnlyBaseName) {
+            $elementKey = Path::classBasename($class);
         }
         
         if ($postProcessor === null) {
-            return $class;
+            return $elementKey;
         }
         
-        return $postProcessor($class);
+        return $postProcessor($class, $elementKey, isset($hasElementKeyProvider));
     }
 }
