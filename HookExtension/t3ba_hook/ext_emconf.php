605f0c17bf6677044801b9b64d704c2abbc50714
Conventional Release Action <conventional.release.action@github.com>
2021-06-27 19:20:47 +0000

    chore(release): 10.1.2 [SKIP CI]

--- HookExtension/t3ba_hook/ext_emconf.php
+++ HookExtension/t3ba_hook/ext_emconf.php
@@ -37,7 +37,7 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 1,
     'lockType' => '',
-    'version' => '10.0.0',
+    'version' => "10.1.2",
     'constraints' => [
         'depends' => [
         ],
